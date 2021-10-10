66996bf0a92dacaf7579331bcaeff98ebd313a0b
osobytes <Ozkar619@gmail.com>
2021-10-10 07:03:04 -0700

    adding github action

--- UnityBotService/Deployment/prepare_secrets.sh
+++ UnityBotService/Deployment/prepare_secrets.sh
@@ -5,11 +5,12 @@ TWITTER_APISECRET=$3
 TWITTER_ACCESSTOKEN=$4
 TWITTER_ACCESSTOKENSECRET=$5
 TWITTER_BEARERTOKEN=$6
-RESOURCE_GROUP=$7
+IMAGE_VERSION=$7
 
 # create parameters json from template.
 cp 'parameters.template.json' 'parameters.json'
 sed -i "s/{ACR_PASSWORD}/${ACR_PASSWORD}/g" parameters.json
+sed -i "s/{IMAGE_VERSION}/${IMAGE_VERSION}/g" parameters.json
 
 ENCODED_TWITTER_APIKEY=$(echo $TWITTER_APIKEY | base64)
 ENCODED_TWITTER_APISECRET=$(echo $TWITTER_APISECRET | base64)
@@ -23,6 +24,3 @@ sed -i "s/{TWITTER_APISECRET}/${ENCODED_TWITTER_APISECRET}/g" template.json
 sed -i "s/{TWITTER_ACCESSTOKEN}/${ENCODED_TWITTER_ACCESSTOKEN}/g" template.json
 sed -i "s/{TWITTER_ACCESSTOKENSECRET}/${ENCODED_TWITTER_ACCESSTOKENSECRET}/g" template.json
 sed -i "s/{TWITTER_BEARERTOKEN}/${ENCODED_TWITTER_BEARERTOKEN}/g" template.json
-
-# use azure CLI to trigger deployment.
-az deployment group create -g $RESOURCE_GROUP -f template.json -p parameters.json
